33f1793324ad567cd202c999a0c61ded02307d32
huangxin <308071066@qq.com>
2016-02-17 21:43:54 +0800

    modify four demo

--- demo4/countwords.py
+++ demo4/countwords.py
@@ -3,9 +3,16 @@
 __author__ = 'huangxin'
 
 import re
+
 with open('words.txt', 'r') as f:
     data = f.read()
-
 result = re.split(r'[\s\,\.]+', data)
 print(result)
 print(len(result)-1)
+
+with open('words.txt', 'r') as f:
+    # for line in f.readlines():
+    #     print(line.strip())
+    words = re.findall(r'[a-zA-Z0-9/’]+', f.read())
+print(words)
+print(len(words))
