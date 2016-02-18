ddffb9d8d00e951244c06d1f7ce4a47105310952
huangxin <308071066@qq.com>
2016-02-18 11:35:50 +0800

    sixth demo

---
+++ demo6/importantwords.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'huangxin'
+import os, re
+#找到该路径下的所有文件
+def get_files(path):
+    filepath = os.listdir(path)
+    files = []
+    for fp in filepath:
+        fppath = path + '/' + fp
+        if os.path.isfile(fppath):
+            files.append(fppath)
+        elif os.path.isdir(fppath):
+            files += get_files(fppath)
+    return files
+
+#去除的词
+stop_word = ['the', 'in', 'of', 'and', 'to', 'has', 'that', 's', 'is', 'are', 'a', 'with', 'as', 'an']
+
+#找到每个词出现的次数
+def get_important_words(files):
+    worddict = {}
+    for filename in files:
+        if filename.endswith('.txt'):
+            with open(filename, 'r') as f:
+                data = f.read()
+                words = re.findall(r'[a-zA-Z0-9/’]+', data)
+                for word in words:
+                    if word not in stop_word:
+                        worddict[word] = worddict[word] + 1 if word in worddict else 1
+    worddict = sorted(worddict.items(), key=lambda x: x[1], reverse=True)
+    return worddict
+
+
+filess = get_files('/Users/huangxin/PycharmProjects/ex/words')
+worddict = get_important_words(filess)
+print(worddict)
